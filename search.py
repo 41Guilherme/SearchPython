a6427b6e14de96cde67702ac9c91cafb6e8b5ae4
41Guilherme <guilherme41@usp.br>
2021-11-17 21:14:41 -0300

    mudança de webbrowser para os

--- search.py
+++ search.py
@@ -1,19 +1,20 @@
 from tkinter import *
-import webbrowser
+import os
 
 win = Tk()
 win.title("Pesquisa Rápida")
 
-label1 = Label(win, text='Insira URL Aqui:  ',font=('arial',10,'bold'))
+label1 = Label(win, text='Faça sua pesquisa ',font=('arial',10,'bold'))
 label1.grid(row=0,column=0)
 
 entry =Entry(win,width=30)
 entry.grid(row=0,column=1)
 
 
+
 def pesquisar():
-    url = entry.get()
-    webbrowser.open(url)
+    os.system("start https://www.google.com/search?q=" + entry.get().replace(' ', '%20'))
+    
 
 
 button = Button(win,text="PESQUISAR", command=pesquisar)
